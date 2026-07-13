25764d7e0281b1a7a48e8eee500bb2000f95f66e
Gabriel Martin <gabrielmartin13009@gmail.com>
2026-07-13 15:27:42 +0200

    feat(cli): support --super flag to grant ROLE_SUPER_ADMIN

--- backend/src/Command/PromoteUserCommand.php
+++ backend/src/Command/PromoteUserCommand.php
@@ -8,10 +8,11 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-#[AsCommand(name: 'app:promote-user', description: 'Grant ROLE_ADMIN to a user by email')]
+#[AsCommand(name: 'app:promote-user', description: 'Grant ROLE_ADMIN (or ROLE_SUPER_ADMIN) to a user by email')]
 class PromoteUserCommand extends Command
 {
     public function __construct(
@@ -24,12 +25,14 @@ class PromoteUserCommand extends Command
     protected function configure(): void
     {
         $this->addArgument('email', InputArgument::REQUIRED, 'Email of the user to promote');
+        $this->addOption('super', null, InputOption::VALUE_NONE, 'Grant ROLE_SUPER_ADMIN instead of ROLE_ADMIN');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io    = new SymfonyStyle($input, $output);
         $email = $input->getArgument('email');
+        $role  = $input->getOption('super') ? 'ROLE_SUPER_ADMIN' : 'ROLE_ADMIN';
 
         $user = $this->userRepository->findOneBy(['email' => $email]);
 
@@ -39,16 +42,16 @@ class PromoteUserCommand extends Command
         }
 
         $roles = $user->getRoles();
-        if (in_array('ROLE_ADMIN', $roles, true)) {
-            $io->warning(sprintf('User "%s" already has ROLE_ADMIN.', $email));
+        if (in_array($role, $roles, true)) {
+            $io->warning(sprintf('User "%s" already has %s.', $email, $role));
             return Command::SUCCESS;
         }
 
-        $roles[] = 'ROLE_ADMIN';
+        $roles[] = $role;
         $user->setRoles(array_values(array_unique($roles)));
         $this->em->flush();
 
-        $io->success(sprintf('User "%s" promoted to ROLE_ADMIN.', $email));
+        $io->success(sprintf('User "%s" promoted to %s.', $email, $role));
         return Command::SUCCESS;
     }
 }
